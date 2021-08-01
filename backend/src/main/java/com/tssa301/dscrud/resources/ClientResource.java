e016acccb5c155e91f5c8e5aadbac128111f0b6d
Tiago Ssa <tiago.silva301@gmail.com>
2021-08-02 00:57:34 +0100

    H2 database

--- backend/src/main/java/com/tssa301/dscrud/resources/ClientResource.java
+++ backend/src/main/java/com/tssa301/dscrud/resources/ClientResource.java
@@ -1,26 +1,26 @@
 package com.tssa301.dscrud.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tssa301.dscrud.entities.Client;
+import com.tssa301.dscrud.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service;
 	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "Alex Grey", "01234567891", 1200.00, null, 2));
-		list.add(new Client(2L, "Ana Bela", "12345678902", 2500.00, null, 1));
-		list.add(new Client(3L, "Mark Cour", "23456789103", 2000.00, null, 2));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
