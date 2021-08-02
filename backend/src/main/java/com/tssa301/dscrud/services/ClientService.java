43f34745ab49a1350778a45350368d1a11209826
Tiago Ssa <tiago.silva301@gmail.com>
2021-08-02 19:19:45 +0100

    ClientDTO

--- backend/src/main/java/com/tssa301/dscrud/services/ClientService.java
+++ backend/src/main/java/com/tssa301/dscrud/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.tssa301.dscrud.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tssa301.dscrud.dto.ClientDTO;
 import com.tssa301.dscrud.entities.Client;
 import com.tssa301.dscrud.repositories.ClientRepository;
 
@@ -16,7 +18,8 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll() {
-		return repository.findAll();
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 }
