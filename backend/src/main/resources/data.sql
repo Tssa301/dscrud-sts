6e40fa46c5062192fe1b2619cb4335fc5e962b4e
Tiago Ssa <tiago.silva301@gmail.com>
2021-08-02 18:42:45 +0100

    DB seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('João Paulo', '12345678901', 1700.0, '1984-02-10T13:30:00Z', 1) 
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria João', '65987231041', 2500.0, '1977-10-06T22:00:00Z', 0) 
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Roberto Vaz', '07958662039', 3000.0, '1980-01-01T16:37:00Z', 2) 
