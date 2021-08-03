880df4774645d6bd0b2b10aa4ce727857f4cc5f9
Tiago Ssa <tiago.silva301@gmail.com>
2021-08-03 20:36:40 +0100

    Auditing data

---
+++ backend/src/main/java/com/tssa301/dscrud/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.tssa301.dscrud.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
